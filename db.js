ddc3d2aa233968d8a8aa7ebbc08fa11324465f07
varshadz <varsha.zurange@inflectionzone.com>
2025-09-12 16:49:27 +0530

    env file added

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels'
+const mongoURL = process.env.DB_URL
 
 //set mongodb connection
 mongoose.connect(mongoURL)
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
     res.send('welcome to our hotel!');
@@ -14,6 +16,6 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on 5000");
 })
